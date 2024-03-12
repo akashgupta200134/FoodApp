a5611d6b018275dbdd9a94ee6017168997d769b8
akashgupta200134 <107178274+akashgupta200134@users.noreply.github.com>
2024-03-12 05:57:24 +0000

    added container

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header";
+import MainContainer from "./components/MainContainer";
+import CreateContainer from "./components/CreateContainer"
 
 
 
 function App() {
   return (
-   <div className=" w-screen h-auto flex flex-col ">
-    <Header></Header>
-    
-   </div>
+    <div className=" w-screen h-auto flex flex-col bg-primary">
+      <Header></Header>
+
+
+      <main className=" w-full p-8 mt-24 ">
+
+        <Routes>
+
+          <Route path="/*" element={<MainContainer />} ></Route>
+          <Route path="/CreateItem" element = {<CreateContainer/>}>
+
+          </Route>
+
+
+
+        </Routes>
+
+
+
+
+      </main>
+
+    </div>
   );
 }
 
